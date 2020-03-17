459cb8c6eb809d4ffa78890ee407d44a71badd4f
Kevin Burleigh <klb@kindling>
2020-03-17 16:53:12 -0500

    adjust access control tests to use Config double

--- app/tests/unit/viewer_request_lambda/test_access_control.py
+++ app/tests/unit/viewer_request_lambda/test_access_control.py
@@ -1,32 +1,9 @@
 import json
 import pytest
 import pdb
+from unittest import mock
 
 from src import viewer_request_lambda_function
-
-##
-## These tests assume the following configuration
-## is stored in the DynamoDb table used by the
-## AWS Lambda function:
-##   {
-##     "access_map": {
-##       "/some-ugly-path/another-ugly-path/biology/biology_book_1.txt": "teacher student anonymous -",
-##       "/some-ugly-path/another-ugly-path/biology/biology_book_2.txt": "teacher student -",
-##       "/some-ugly-path/another-ugly-path/biology/biology_book_3.txt": "teacher -",
-##       "/some-ugly-path/another-ugly-path/biology/biology_book_4.txt": "-"
-##     },
-##     "config_id": "ver_1",
-##     "uri_map": {
-##       "/biology/vers/1": "/some-ugly-path/another-ugly-path/biology/biology_book_1.txt",
-##       "/biology/vers/2": "/some-ugly-path/another-ugly-path/biology/biology_book_2.txt",
-##       "/biology/vers/3": "/some-ugly-path/another-ugly-path/biology/biology_book_3.txt",
-##       "/biology/vers/4": "/some-ugly-path/another-ugly-path/biology/biology_book_4.txt",
-##       "/biology/vers/5": "/some-ugly-path/another-ugly-path/biology/biology_book_5.txt",
-##       "/biology/vers/latest": "/biology/vers/2",
-##       "/some-ugly-path/another-ugly-path/biology/biology_book_1.txt": "/some-ugly-path/another-ugly-path/biology/biology_book_1.txt"
-##     }
-##   }
-##
 
 def load_event(event_name, **data):
     from mako.template import Template
@@ -40,84 +17,38 @@ def load_event(event_name, **data):
     event = json.loads(event_text)
     return event
 
-def test_access_denied_teacher_missing_acl():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/5',
-        cookies=['user=teacher'],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['status'] == 403
+def test_missing_ugly_uri_gives_response_with_status_404():
+    with mock.patch('src.viewer_request_lambda_function.Config') as MockConfig:
+        MockConfig.return_value.get_ugly_uri.return_value = None
+        MockConfig.return_value.access_is_allowed.return_value = True
+        event = load_event('access_tests_template',
+            uri='/some/pretty/uri',
+            cookies=['user=some_user'],
+        )
+        context = ''
+        response = viewer_request_lambda_function.lambda_handler(event, context)
+        assert response['status'] == 404
 
-def test_access_denied_student_missing_acl():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/5',
-        cookies=['user=student'],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['status'] == 403
+def test_access_denied_gives_response_with_status_403():
+    with mock.patch('src.viewer_request_lambda_function.Config') as MockConfig:
+        MockConfig.return_value.get_ugly_uri.return_value = '/some/ugly/uri'
+        MockConfig.return_value.access_is_allowed.return_value = False
+        event = load_event('access_tests_template',
+            uri='/some/pretty/uri',
+            cookies=['user=some_user'],
+        )
+        context = ''
+        response = viewer_request_lambda_function.lambda_handler(event, context)
+        assert response['status'] == 403
 
-def test_access_denied_anonymous_missing_acl():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/5',
-        cookies=[],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['status'] == 403
-
-def test_access_denied_teacher_not_in_acl():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/4',
-        cookies=['user=teacher'],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['status'] == 403
-
-def test_access_denied_student_not_in_acl():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/3',
-        cookies=['user=student'],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['status'] == 403
-
-def test_access_denied_anonymous_not_in_acl():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/2',
-        cookies=[],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['status'] == 403
-
-def test_access_granted_teacher():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/3',
-        cookies=['user=teacher'],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['uri'] == '/some-ugly-path/another-ugly-path/biology/biology_book_3.txt'
-
-def test_access_granted_student():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/2',
-        cookies=['user=student'],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['uri'] == '/some-ugly-path/another-ugly-path/biology/biology_book_2.txt'
-
-def test_access_granted_anonymous():
-    event = load_event('access_tests_template',
-        uri='/biology/vers/1',
-        cookies=[],
-    )
-    context = ''
-    response = viewer_request_lambda_function.lambda_handler(event, context)
-    assert response['uri'] == '/some-ugly-path/another-ugly-path/biology/biology_book_1.txt'
-
+def test_access_allowed_gives_request_with_ugly_uri():
+    with mock.patch('src.viewer_request_lambda_function.Config') as MockConfig:
+        MockConfig.return_value.get_ugly_uri.return_value = '/some/ugly/uri'
+        MockConfig.return_value.access_is_allowed.return_value = True
+        event = load_event('access_tests_template',
+            uri='/some/pretty/uri',
+            cookies=['user=some_user'],
+        )
+        context = ''
+        request = viewer_request_lambda_function.lambda_handler(event, context)
+        assert request['uri'] == '/some/ugly/uri'
