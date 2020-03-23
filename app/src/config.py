35f09b0eae6df44ed1b3a19ba1ae80a256eb2dad
Kevin Burleigh <klb@kindling>
2020-03-23 05:29:41 -0500

    add get_version() method; extract get_configs_from_dynamodb() static method

--- app/src/config.py
+++ app/src/config.py
@@ -5,25 +5,10 @@ class Config(object):
         self.region_name = region_name
         self.table_name  = table_name
 
-        session = boto3.session.Session(region_name=self.region_name)
-        ddb_client = session.client('dynamodb')
-
-        ## Fetch all configs with is_current set to true.
-        ## NOTE: This assumes that DynamoDb does not
-        ##       process more than 1MB of data to find
-        ##       the desired items.
-        ddb_response = ddb_client.scan(
-            TableName=self.table_name,
-            ExpressionAttributeValues={
-                ':c': {
-                    'BOOL': True,
-                }
-            },
-            FilterExpression='is_current = :c',
+        configs = Config.get_configs_from_dynamodb(
+            region_name=self.region_name,
+            table_name=self.table_name,
         )
-        configs = ddb_response['Items']
-        if len(configs) == 0:
-            raise RuntimeError('unable to find any table items with is_current == true')
 
         ## The highest version numbered current config
         ## is the one we want.
@@ -34,6 +19,9 @@ class Config(object):
         config = sorted(configs, key=get_version_number)[-1]
 
         self.config_data = config
+
+    def get_version(self):
+        return self.config_data['config_id']['S']
 
     def get_ugly_uri(self, pretty_uri):
         ugly_uri = None
@@ -56,3 +44,34 @@ class Config(object):
                       .get('S', '-') \
                       .strip().split()
         return user in allowed
+
+    @staticmethod
+    def get_configs_from_dynamodb(region_name, table_name):
+        session = boto3.session.Session(region_name=region_name)
+        ddb_client = session.client('dynamodb')
+
+        try:
+            ## Fetch all configs with is_current set to true.
+            ## NOTE: This assumes that DynamoDb does not
+            ##       process more than 1MB of data to find
+            ##       the desired items.
+            ddb_response = ddb_client.scan(
+                TableName=table_name,
+                ExpressionAttributeValues={
+                    ':c': {
+                        'BOOL': True,
+                    }
+                },
+                FilterExpression='is_current = :c',
+            )
+            configs = ddb_response['Items']
+        except Exception as error:
+            raise RuntimeError('could not get configs (region_name={}, table_name={}): {}'.format(
+                region_name,
+                table_name,
+                str(error),
+            ))
+
+        if len(configs) == 0:
+            raise RuntimeError('unable to find any table items with is_current == true')
+        return configs
