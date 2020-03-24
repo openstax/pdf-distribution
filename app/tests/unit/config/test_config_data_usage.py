acf4bfb970dd00ff0a0097e7a186e36707051a1e
Kevin Burleigh <klb@kindling>
2020-03-24 02:56:24 -0500

    DRY up tests by creating default template arguments

--- app/tests/unit/config/test_config_data_usage.py
+++ app/tests/unit/config/test_config_data_usage.py
@@ -3,37 +3,46 @@ from src.config import Config
 import json
 from unittest import mock
 
-def load_config(config_name, **data):
+def create_config(**data):
     from mako.template import Template
 
-    with open('./tests/unit/config/{}.json'.format(config_name)) as fd:
-        template_text = fd.read()
+    CONFIG_TEMPLATE = """
+    {
+      "access_map": {
+        "M": ${access_map}
+      },
+      "config_id": {
+        "S": ${version}
+      },
+      "is_current": {
+        "BOOL": true
+      },
+      "uri_map": {
+        "M": ${uri_map}
+      }
+    }
+    """
+
+    defaults = {
+        'access_map': {},
+        'version':    'ver_42',
+        'uri_map':    {},
+    }
+
+    data          = {**defaults, **data}
+    template_text = CONFIG_TEMPLATE
 
     config_text = Template(template_text).render(**data)
     config = json.loads(config_text)
+
     return config
 
 def test_highest_config_version_chosen():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({}),
-                version     = json.dumps('ver_1'),
-                uri_map     = json.dumps({})
-            ),
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({}),
-                version     = json.dumps('ver_10'),
-                uri_map     = json.dumps({})
-            ),
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({}),
-                version     = json.dumps('ver_2'),
-                uri_map     = json.dumps({})
-            )
+            create_config(version=json.dumps('ver_1')),
+            create_config(version=json.dumps('ver_10')),
+            create_config(version=json.dumps('ver_2')),
         ]
 
         config = Config(
@@ -46,11 +55,9 @@ def test_highest_config_version_chosen():
 def test_uri_map_missing_pretty_uri():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({}),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({
+            create_config(
+                version = json.dumps('ver_42'),
+                uri_map = json.dumps({
                     '/some-pretty-path/pretty-resource': {
                         'S': '/some-ugly-path/ugly-resource'
                     }
@@ -69,11 +76,9 @@ def test_uri_map_missing_pretty_uri():
 def test_uri_map_is_pretty_uri():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({}),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({
+            create_config(
+                version = json.dumps('ver_42'),
+                uri_map = json.dumps({
                     '/some-ugly-path/ugly-resource': {
                         'S': '/some-ugly-path/ugly-resource'
                     }
@@ -93,11 +98,9 @@ def test_uri_map_is_pretty_uri():
 def test_uri_map_direct_lookup():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({}),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({
+            create_config(
+                version = json.dumps('ver_42'),
+                uri_map = json.dumps({
                     '/some-pretty-path/pretty-resource': {
                         'S': '/some-ugly-path/ugly-resource'
                     }
@@ -116,11 +119,9 @@ def test_uri_map_direct_lookup():
 def test_uri_map_indirection():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({}),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({
+            create_config(
+                version = json.dumps('ver_42'),
+                uri_map = json.dumps({
                     '/some-pretty-path/pretty-resource': {
                         'S': '/another-pretty-path/another-resource'
                     },
@@ -142,12 +143,7 @@ def test_uri_map_indirection():
 def test_access_map_missing_ugly_uri():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({}),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({})
-            )
+            create_config(version=json.dumps('ver_42')),
         ]
 
         config = Config(
@@ -165,15 +161,13 @@ def test_access_map_missing_ugly_uri():
 def test_access_map_no_entries():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({
+            create_config(
+                access_map = json.dumps({
                     '/some-ugly-path/ugly-resource': {
                         'S': '-'
                     }
                 }),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({})
+                version    = json.dumps('ver_42'),
             )
         ]
 
@@ -192,15 +186,13 @@ def test_access_map_no_entries():
 def test_access_map_single_entry_allowed():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
+            create_config(
                 access_map  = json.dumps({
                     '/some-ugly-path/ugly-resource': {
                         'S': 'user_category -'
                     }
                 }),
                 version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({})
             )
         ]
 
@@ -219,15 +211,13 @@ def test_access_map_single_entry_allowed():
 def test_access_map_single_entry_disallowed():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({
+            create_config(
+                access_map = json.dumps({
                     '/some-ugly-path/ugly-resource': {
                         'S': 'user_category_1 -'
                     }
                 }),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({})
+                version    = json.dumps('ver_42'),
             )
         ]
 
@@ -246,15 +236,13 @@ def test_access_map_single_entry_disallowed():
 def test_access_map_multiple_allowed():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({
+            create_config(
+                access_map = json.dumps({
                     '/some-ugly-path/ugly-resource': {
                         'S': 'user_category_1 user_category_2 user_category_3 -'
                     }
                 }),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({})
+                version    = json.dumps('ver_42'),
             )
         ]
 
@@ -273,15 +261,13 @@ def test_access_map_multiple_allowed():
 def test_access_map_multiple_disallowed():
     with mock.patch('src.config.Config.get_configs_from_dynamodb') as mock_dynamo:
         mock_dynamo.return_value = [
-            load_config(
-                config_name = 'config_template',
-                access_map  = json.dumps({
+            create_config(
+                access_map = json.dumps({
                     '/some-ugly-path/ugly-resource': {
                         'S': 'user_category_1 user_category_2 user_category_3 -'
                     }
                 }),
-                version     = json.dumps('ver_42'),
-                uri_map     = json.dumps({})
+                version    = json.dumps('ver_42'),
             )
         ]
 
